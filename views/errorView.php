36d9cbae0560bba55626430703671c92481029a5
Florent THOMAS <florentthomas78@gmail.com>
2021-02-14 20:27:36 +0100

    ajout style sur la page erreur

--- views/errorView.php
+++ views/errorView.php
@@ -1,3 +1,6 @@
 
 
-<p><?=$error_message?></p>
+<div class="text-center"><img id="image_erreur" src="<?=URL?>public/images/erreur.png" alt="image_d'erreur"/></div>
+
+<p class="text-center">Oups il semble avoir une erreur<br /><?=$error_message?></p>
+<div class="text-center"><a href="<?=URL?>"><button type="button" class="btn btn-primary">Revenir au blog</button></a></div>
