8ebfdfed66d8847642b7bff4b023b7b25a684160
Florent THOMAS <florentthomas78@gmail.com>
2021-03-21 05:06:31 +0100

    methode get_user dans userManager

--- models/UserManager.php
+++ models/UserManager.php
@@ -9,13 +9,9 @@ class UserManager extends Manager{
 
     public function get_user($email,$password){
 
-        $sql="SELECT * FROM users WHERE email=:email";
+        $respons=$this->get_item('users','email',$email);
 
-        $req=$this->bdd()->prepare($sql);
-
-        $req->execute(array(':email' => $email));
-        
-        $user=$req->fetch(\PDO::FETCH_ASSOC);
+        $user=$respons->fetch(\PDO::FETCH_ASSOC);
 
         $password_check=password_verify($password,$user['password_user']);
 
